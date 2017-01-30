923e002c52df1be416aa7559d1060986a4d85d0b
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-01-30 15:10:23 -0800

    getting mining fee support

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 module.exports = function (baseUri) {
-
-  var util = {}
   var insight  = require("./src/insight")(baseUri)
   var btcTuner = require("./src/btctuner")(baseUri, insight)
   btcTuner.init()
-  util =  {
-    subscribe             : btcTuner.subscribe,
-    unsubscribe           : btcTuner.unsubscribe,
-    sync                  : insight.sync,
-    getAddress            : insight.getAddress,
-    getBalanceFromUnspents: insight.getBalanceFromUnspents,
-    getUnspents           : insight.getUnspents,
-    getConfirmedUnspents  : insight.getConfirmedUnspents,
-    getTransaction        : insight.getTransaction,
-    sendTransaction       : insight.sendTransaction,
-    multi                 : insight.multi
-  }
-
-  return util
+  insight.subscribe   = btcTuner.subscribe
+  insight.unsubscribe = btcTuner.unsubscribe
+  return insight
 }
